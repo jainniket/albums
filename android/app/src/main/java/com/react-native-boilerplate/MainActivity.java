bc4b24ebf9f2c80d3aab7b05882d1ec650dfc645
Niket Jain <niket_jain@mckinsey.com>
2017-03-03 16:24:29 +0530

    changed the name

--- android/app/src/main/java/com/react-native-boilerplate/MainActivity.java
+++ android/app/src/main/java/com/react-native-boilerplate/MainActivity.java
@@ -1,4 +1,4 @@
-package com.albums;
+package com.react-native-boilerplate;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
      */
     @Override
     protected String getMainComponentName() {
-        return "albums";
+        return "react-native-boilerplate";
     }
 }
