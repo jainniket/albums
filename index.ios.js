ccb0397a018f846c8c5a9f1e7ea0376b3cbf371b
Niket Jain <niket.jain1210@gmail.com>
2017-03-19 18:52:16 +0700

    Feature/name change (#2)
    * changed the name
    
    * changed the app name

--- index.ios.js
+++ index.ios.js
@@ -3,4 +3,4 @@
 import { AppRegistry } from 'react-native';
 import App from './app/app';
 
-AppRegistry.registerComponent('albums', () => App);
+AppRegistry.registerComponent('reactNativeBoilerplate', () => App);
